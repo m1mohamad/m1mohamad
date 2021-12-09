cfcef3879cd879b231298091d57baca79184cbfd
Dayyeh <dayyehm@amazon.com>
2021-12-09 12:28:33 +0200

    Initial commit

---
+++ tests/unit/test_cdk_debug_project_stack.py
@@ -0,0 +1,19 @@
+# from aws_cdk import (
+#         core,
+#         assertions
+#     )
+
+# from cdk_debug_project.cdk_debug_project_stack import CdkDebugProjectStack
+
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in cdk_debug_project/cdk_debug_project_stack.py
+def test_sqs_queue_created():
+#     app = core.App()
+#     stack = CdkDebugProjectStack(app, "cdk-debug-project")
+#     template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
+  pass
